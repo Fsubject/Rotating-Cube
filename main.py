9eb05c67969a5631294afadebbd16a6c66efa658
Fsubject <none>
2024-09-15 12:49:53 +0200

    adding text redenring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pygame
-
 from settings import *
 
 
@@ -33,7 +32,8 @@ def connect_points(window, points_pos):
     pygame.draw.line(window, GREEN, (points_pos[7][0], points_pos[7][1]), (points_pos[4][0], points_pos[4][1]))
 
     for i in range(7):
-        pygame.draw.line(window, GREEN, (points_pos[i][0], points_pos[i][1]), (points_pos[i + 1][0], points_pos[i + 1][1]))
+        pygame.draw.line(window, GREEN, (points_pos[i][0], points_pos[i][1]),
+                         (points_pos[i + 1][0], points_pos[i + 1][1]))
 
     pygame.draw.line(window, GREEN, (points_pos[0][0], points_pos[0][1]), (points_pos[7][0], points_pos[7][1]))
     pygame.draw.line(window, GREEN, (points_pos[1][0], points_pos[1][1]), (points_pos[6][0], points_pos[6][1]))
@@ -49,6 +49,7 @@ def connect_points(window, points_pos):
 
 def main():
     pygame.init()
+    pygame.font.init()
 
     window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
@@ -56,7 +57,17 @@ def main():
 
     pygame.display.set_caption("Rotating cube")
 
+    # Font settings / texts
+    nice_font = pygame.font.Font("resources/Silkscreen-Regular.ttf", 25)
+    texts = [nice_font.render("[C] Toggle cube points", False, GREEN),
+             nice_font.render("[UP] Increase rotation speed", False, GREEN),
+             nice_font.render("[DOWN] Decrease rotation speed", False, GREEN),
+             nice_font.render("[Z] Scale up cube", False, GREEN),
+             nice_font.render("[S] Scale down cube", False, GREEN)]
+
+    # Cube settings
     cube_scale = 120
+    toggle_draw_point = True
     angle_x = 0
     angle_y = 0
     angle_z = 0
@@ -81,6 +92,11 @@ def main():
                 elif event.key == pygame.K_DOWN:
                     rotation_speed -= 0.01
                     rotation_speed -= 0.01
+                elif event.key == pygame.K_c:
+                    if toggle_draw_point:
+                        toggle_draw_point = False
+                    else:
+                        toggle_draw_point = True
 
         rotation_x_matrix, rotation_y_matrix, rotation_z_matrix = create_rotations_matrices(angle_x, angle_y, angle_z)
 
@@ -101,11 +117,17 @@ def main():
             x = (point[0] * cube_scale) + WIN_WIDTH / 2
             y = (point[1] * cube_scale) + WIN_HEIGHT / 2
 
-            points_pos.append((int(x), int(y))) # Keep track of the points position
+            points_pos.append((int(x), int(y)))  # Keep track of the points position
 
-            pygame.draw.circle(window, WHITE, (x, y), 3) # Draw a point of the cube
+            if toggle_draw_point:
+                pygame.draw.circle(window, WHITE, (x, y), 3)  # Draw a point of the cube
 
         connect_points(window, points_pos)
+
+        i = 0
+        for text in texts:
+            i += 30
+            window.blit(text, (40, 10 + i))
 
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -1,12 +1,15 @@
 import numpy as np
 
+# Window settings
 WIN_WIDTH, WIN_HEIGHT = 1200, 800
 
+# Color
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
+# Matrices
 projection_matrix = np.array([
     [1, 0, 0],
     [0, 1, 0],
